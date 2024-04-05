935734bef03e065c523da0fb62956e8ba13762e5
Dariusz Okoński <darek200180@gmail.com>
2024-04-05 19:30:12 +0200

    fix Services

--- src/data/data.js
+++ src/data/data.js
@@ -10,3 +10,24 @@ export const socialLinks = [
   { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
   { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sint reprehenderit eos nostrum laboriosam ullam molestiae fugiat porro? Voluptatum quasi expedita amet ea deserunt error modi incidunt obcaecati, corrupti tenetur.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, totam ipsam? Accusamus officia corporis praesentium!',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis rem placeat quia vitae eaque, sunt dolor consectetur magni nulla fugit.',
+  },
+];
