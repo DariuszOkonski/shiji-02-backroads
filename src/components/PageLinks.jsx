476cd398ab463d72831a604447b8915c3d379f0c
Dariusz Okoński <darek200180@gmail.com>
2024-04-05 20:33:24 +0200

    add PageLinks

--- src/components/PageLinks.jsx
+++ src/components/PageLinks.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { pageLinks } from '../data/data';
+import PageLink from './PageLink';
 
 function PageLinks({ parentClass, itemClass }) {
   return (
     <ul className={parentClass} id='nav-links'>
       {pageLinks.map((link) => (
-        <li key={link.id}>
-          <a href={link.href} className={itemClass}>
-            {link.text}
-          </a>
-        </li>
+        <PageLink key={link.id} {...link} itemClass={itemClass} />
       ))}
     </ul>
   );
